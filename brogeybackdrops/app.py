2e2f9756e42e7dd2e4ac66d6d90236b9235bb7e4
shanayabarretto <barretto.shanaya@gmail.com>
2022-04-11 00:53:41 -0400

    made database, working on recognizing codes

--- brogeybackdrops/app.py
+++ brogeybackdrops/app.py
@@ -1,10 +1,14 @@
 import os
 import imghdr
+from cs50 import SQL
 from flask import Flask, abort, current_app, render_template, request, redirect
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.jpeg']
 app.config['UPLOAD_PATH'] = 'static/userImages'
+db = SQL("sqlite:///userdata.db")
+
+code = "start"
 
 def validate_image(stream):
     header = stream.read(512)
@@ -14,11 +18,11 @@ def validate_image(stream):
         return None
     return '.' + (format if format != 'jpeg' else 'jpg')
 
-#defaultUrl = "https://variety.com/wp-content/uploads/2020/01/taylor-swift-variety-cover-5-16x9-1000.jpg?w=681&h=383&crop=1"
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
     if request.method == "GET":
-        return render_template("index.html") #, url=defaultUrl
+        picture = db.execute("SELECT photoPath FROM basicdata WHERE code=:code", code=code)
+        return render_template("index.html", picture=picture)
     else:
         image = request.files['imageFile']
         if image.filename != '':
@@ -27,5 +31,8 @@ def hello_world():
                 file_ext != validate_image(image.stream):
                 abort(400)
             image.save(os.path.join(app.config['UPLOAD_PATH'], image.filename))
+            #code = "no"
+            #photoPath = app.config['UPLOAD_PATH'] + "/" + image.filename
+            #db.execute("INSERT INTO basicdata (code, photoPath) VALUES (:code, :photoPath)", code=code, photoPath=photoPath)
         return redirect("/")
         
