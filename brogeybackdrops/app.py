48fd042ecc458751bf7fbe585e4adc1f9a2205d7
shanayabarretto <barretto.shanaya@gmail.com>
2022-04-10 23:44:53 -0400

    safe uploads

--- brogeybackdrops/app.py
+++ brogeybackdrops/app.py
@@ -1,5 +1,18 @@
-from flask import Flask, render_template, request, redirect
+import os
+import imghdr
+from flask import Flask, abort, current_app, render_template, request, redirect
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.jpeg']
+app.config['UPLOAD_PATH'] = 'static/userImages'
+
+def validate_image(stream):
+    header = stream.read(512)
+    stream.seek(0) 
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
 
 #defaultUrl = "https://variety.com/wp-content/uploads/2020/01/taylor-swift-variety-cover-5-16x9-1000.jpg?w=681&h=383&crop=1"
 @app.route('/', methods=["GET", "POST"])
@@ -9,6 +22,10 @@ def hello_world():
     else:
         image = request.files['imageFile']
         if image.filename != '':
-            image.save("./userImages/" + image.filename)
+            file_ext = os.path.splitext(image.filename)[1]
+            if file_ext not in current_app.config['UPLOAD_EXTENSIONS'] or \
+                file_ext != validate_image(image.stream):
+                abort(400)
+            image.save(os.path.join(app.config['UPLOAD_PATH'], image.filename))
         return redirect("/")
         
