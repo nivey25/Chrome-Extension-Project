aec347988761989fcffeb35429a0f4bf9145cd7e
shanayabarretto <barretto.shanaya@gmail.com>
2022-04-10 23:01:47 -0400

    adds user inputted file to directory

--- brogeybackdrops/app.py
+++ brogeybackdrops/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-@app.route('/')
+
+#defaultUrl = "https://variety.com/wp-content/uploads/2020/01/taylor-swift-variety-cover-5-16x9-1000.jpg?w=681&h=383&crop=1"
+@app.route('/', methods=["GET", "POST"])
 def hello_world():
-    name = "shan"
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html") #, url=defaultUrl
+    else:
+        image = request.files['imageFile']
+        if image.filename != '':
+            image.save("./userImages/" + image.filename)
+        return redirect("/")
+        
